e97ec3d0f469a1be014dc0a2018c4470ce168d0a
Anuraag Kamath <anuraag.kamath@gmail.com>
2018-08-21 16:43:17 +0530

    login change css

--- public/others/js/workitems.js
+++ public/others/js/workitems.js
@@ -30,16 +30,16 @@ fetch('/workitems?search={"status":"scheduled"}', {
 
                 row.title = "Escalated on " + alpha[i].escalationDate
                 var wi_status = alpha[i].status;
-                
-                if(alpha[i].currentStatus!=undefined && alpha[i].currentStatus!=='undefined' && alpha[i].currentStatus=="open"){
+
+                if (alpha[i].currentStatus != undefined && alpha[i].currentStatus !== 'undefined' && alpha[i].currentStatus == "open") {
                     wi_status = alpha[i].currentStatus;
-                    if(alpha[i].currentUser==loggedInUser){
-                        wi_status+=" by you"
-                    }else{
-                        wi_status+=" by some other user"
+                    if (alpha[i].currentUser == loggedInUser) {
+                        wi_status += " by you"
+                    } else {
+                        wi_status += " by some other user"
                     }
                 }
-                row.innerHTML = "<td>" + (i + 1) + "</td><td class='editUsers' id=" + alpha[i]._id + " instanceId=" + alpha[i].instanceId + "><u title='Click me to get all the objects associated with the selected workitem and instance!'>" + alpha[i]._id + "</u></td><td  class='editUsers'  id='" + alpha[i].instanceId + "'><u title='Click me to get to know the current status of the instance!'>" + alpha[i].instanceId + "</u></td><td>" + alpha[i].processName + "</td><td>" + alpha[i].stepName + "</td><td>" + wi_status + "</td><td>" + alpha[i].date + "</td><td class='trigger' workitemId='" + alpha[i]._id + "'><a href='#" + alpha[i]._id + "'><h3><i class='far fa-play-circle'></i></h3></a></td>"
+                row.innerHTML = "<td class='smallScreen'>" + (i + 1) + "</td><td><u  class='editUsers' id=" + alpha[i]._id + " instanceId=" + alpha[i].instanceId + "  title='Click me to get all the objects associated with the selected workitem and instance!'>" + alpha[i]._id + "</u></td><td class='smallScreen'><u  class='editUsers'  id='" + alpha[i].instanceId + "' title='Click me to get to know the current status of the instance!'>" + alpha[i].instanceId + "</u></td><td class='smallScreen'>" + alpha[i].processName + "</td><td class='smallScreen'>" + alpha[i].stepName + "</td><td class='smallScreen'>" + wi_status + "</td><td class='smallScreen'>" + alpha[i].date + "</td><td class='trigger' workitemId='" + alpha[i]._id + "'><a href='#" + alpha[i]._id + "'><h3><i class='far fa-play-circle'></i></h3></a></td>"
 
                 // {
 
@@ -89,6 +89,14 @@ fetch('/workitems?search={"status":"scheduled"}', {
 
                     fetch('/searchObjects/' + document.getElementById(ev.target.id).getAttribute('instanceId'), { credentials: "include" }).then((prom) => prom.text()).then((res) => {
                         document.getElementById("objectWorkitemDiv").innerHTML = "";
+                        var closeObjectWorkitemDiv = document.createElement("BUTTON");
+                        closeObjectWorkitemDiv.id = "closeObjectWorkitemDiv";
+                        closeObjectWorkitemDiv.innerText = "X";
+                        closeObjectWorkitemDiv.className = "right btn"
+                        document.getElementById("objectWorkitemDiv").appendChild(closeObjectWorkitemDiv)
+
+                        console.log(document.getElementById("closeObjectWorkitemDiv"));
+
                         res = JSON.parse(res);
                         if (res.length > 0) {
                             for (var i = 0; i < res.length; i++) {
@@ -102,6 +110,12 @@ fetch('/workitems?search={"status":"scheduled"}', {
                             document.getElementById("objectWorkitemDiv").innerHTML = "<h3>No Objects associated with this instance</h3>";
 
                         }
+                        document.getElementById("closeObjectWorkitemDiv").addEventListener('click', (ev) => {
+                            console.log("HITME");
+                            document.getElementById('workitemsDiv').style.height = "100%";
+                            document.getElementById('objectWorkitemDiv').style.height = "0%";
+
+                        });
                     })
                 });
             }
@@ -122,20 +136,21 @@ addTable = (name, id, tid) => {
     }).then((prom) => prom.text()).then((res3) => {
         res3 = JSON.parse(res3);
         var headers = Object.keys((res3[0][name][0]));
-        for (var k = 0; k < headers.length; k++) {
+        for (var k = 1; k < headers.length; k++) {
             var th = document.createElement("TH");
             th.innerText = headers[k];
             document.getElementById("obj_" + tid).appendChild(th)
         }
         for (var m = 0; m < res3[0][name].length; m++) {
             var tr = document.createElement("TR");
-            for (var n = 0; n < headers.length; n++) {
+            for (var n = 1; n < headers.length; n++) {
                 var td = document.createElement("TD");
                 td.innerText = res3[0][name][m][headers[n]];
                 tr.appendChild(td)
 
             }
             document.getElementById("obj_" + tid).appendChild(tr);
+            document.getElementById("obj_" + tid).className = "table"
         }
     })
 }
